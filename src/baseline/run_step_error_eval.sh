f17f0c56262e904352b42da7ebafb1f117d4a126
Sagnik Mukherjee <sagnikm3@convai-srv-03.cs.illinois.edu>
2025-08-26 20:00:31 -0500

    initial commit

--- src/baseline/run_step_error_eval.sh
+++ src/baseline/run_step_error_eval.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
 
-MODEL_PATH="meta-llama/Llama-3.1-70B-Instruct"
+MODEL_PATH="meta-llama/Llama-3.1-8B-Instruct"
 
 # Input and output files
 
 INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_negatives.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_llama70b.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_llama8b.json"
+
+# Run the error classification script
+python "step_error_eval.py" \
+    --input-file "$INPUT_FILE" \
+    --output-file "$OUTPUT_FILE" \
+    --model-path "$MODEL_PATH" \
+    --tensor-parallel-size 4
+
+INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_negatives_cleaned.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_cleaned_llama8b.json"
 
 # Run the error classification script
 python "step_error_eval.py" \
@@ -17,7 +27,7 @@ python "step_error_eval.py" \
 
 
 INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_llama70b.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_llama8b.json"
 
 # Run the error classification script
 python "step_error_eval.py" \
@@ -26,8 +36,8 @@ python "step_error_eval.py" \
     --model-path "$MODEL_PATH" \
     --tensor-parallel-size 4
 
-INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives_perturbed.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_perturbed_llama70b.json"
+INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives_cleaned.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_cleaned_llama8b.json"
 
 # Run the error classification script
 python "step_error_eval.py" \
--- src/error_eval/merged_eval_gsm8k.sh
+++ src/error_eval/merged_eval_gsm8k.sh
@@ -1,6 +1,17 @@
-MODEL_PATH="meta-llama/Llama-3.1-70B-Instruct"
+MODEL_PATH="meta-llama/Llama-3.1-8B-Instruct"
+
+
 INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_negatives.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_llama70b.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_llama8b.json"
+
+python merged_premise_error_eval.py \
+    --input-file "$INPUT_FILE" \
+    --output-file "$OUTPUT_FILE" \
+    --model-path "$MODEL_PATH" \
+    --tensor-parallel-size 4
+
+INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_negatives_cleaned.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_negatives_cleaned_llama8b.json"
 
 python merged_premise_error_eval.py \
     --input-file "$INPUT_FILE" \
@@ -9,7 +20,7 @@ python merged_premise_error_eval.py \
     --tensor-parallel-size 4
 
 INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_llama70b.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_llama8b.json"
 
 python merged_premise_error_eval.py \
     --input-file "$INPUT_FILE" \
@@ -17,11 +28,12 @@ python merged_premise_error_eval.py \
     --model-path "$MODEL_PATH" \
     --tensor-parallel-size 4
 
-INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives_perturbed.json"
-OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_perturbed_llama70b.json"
+INPUT_FILE="/home/sagnikm3/PARC/datasets/gsm8k/gsm8k_positives_cleaned.json"
+OUTPUT_FILE="/home/sagnikm3/PARC/outputs/gsm8k_positives_cleaned_llama8b.json"
 
 python merged_premise_error_eval.py \
     --input-file "$INPUT_FILE" \
     --output-file "$OUTPUT_FILE" \
     --model-path "$MODEL_PATH" \
     --tensor-parallel-size 4
+
